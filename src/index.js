6d9a2fe5737b90bc9b51b0718b88f5b35a422f19
grzegorzg <grzegorz@devstream.pl>
2018-11-24 09:32:45 +0100

    App setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import { Root}  from './containers'
+import { configure } from 'mobx';
+
+configure({ enforceActions: 'observed' });
+
+ReactDom.render(<Root />, document.getElementById('root'))
