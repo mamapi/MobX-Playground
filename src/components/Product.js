bc1d3f8c18bd77ca3eb011b3663a5a57e8e9328d
grzegorzg <grzegorz@devstream.pl>
2018-11-24 10:13:29 +0100

    Added Buy button

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,21 @@
 import React from 'react'
 
-export const Product = ({ name }) => (
-    <span>{name}</span>
-)
+export const Product = ({ id, name, tags, isSold, onBuyClick }) => {
+
+    const handleClick = () => {
+        onBuyClick(id)
+    }
+
+    return (
+        <div>
+            <span style={{ textDecoration: isSold ? 'line-through' : 'none' }}>{name}: </span>
+            <button onClick={handleClick}>
+                Buy
+        </button>
+            {tags.map(tag =>
+                <p key={tag}>{tag}</p>
+            )}
+        </div>
+    )
+}
+
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,9 +4,16 @@ import { Product } from './Product'
 export class ProductList extends Component {
     state = {
         products: [
-            { id: 0, name: 'Apple' },
-            { id: 1, name: 'Cofee' },
+            { id: 0, name: 'Apple', tags: ['Red', 'Green'], isSold: false },
+            { id: 1, name: 'Cofee', tags: ['White', 'Black'], isSold: false }
         ]
+    }
+
+    handleBuyClick = id => {
+        this.setState(previousState => {
+            const products = previousState.products.map(p => p.id === id ? { ...p, isSold: true } : { ...p })
+            return { products }
+        })
     }
 
     render() {
@@ -15,10 +22,10 @@ export class ProductList extends Component {
             <ul>
                 {products.map(product => (
                     <li key={product.id}>
-                        <Product  {...product} />
+                        <Product  {...product} onBuyClick={this.handleBuyClick} />
                     </li>
                 ))}
             </ul>
         )
     }
-} 
+}
